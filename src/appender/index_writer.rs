e53246945e50dfbf65c405a7c0f6b4963226f438
ho-229 <56023510+ho-229@users.noreply.github.com>
2023-02-02 22:52:29 +0800

    feat: update header of log file
    chore

--- src/appender/index_writer.rs
+++ src/appender/index_writer.rs
@@ -1,54 +1,49 @@
 use anyhow::{anyhow, ensure, Result};
 use crossbeam::channel::{self, Receiver, Sender};
+use positioned_io::ReadAt;
 use serde::Serialize;
-use std::{
-    fmt::Display,
-    fs::File,
-    io::Write,
-    mem::size_of,
-    path::Path,
-    thread::{self, JoinHandle},
-};
+use std::{fs::File, io::Write, mem::size_of, path::Path, thread};
+
+use crate::formats::log::IndexFileHeader;
 
 #[derive(Debug)]
-pub struct IndexWriter<T: Serialize + Send + Sync + 'static> {
-    handle: JoinHandle<Result<()>>,
+pub(crate) struct IndexWriter<T: Serialize + Send + Sync + 'static> {
     sender: Sender<Vec<T>>,
 }
 
 impl<T: Serialize + Send + Sync + 'static> IndexWriter<T> {
-    pub fn new(
-        path: impl AsRef<Path>,
-        file_name: impl Display,
-        suffix: impl Display,
-    ) -> Result<Self> {
+    pub(crate) fn new(path: impl AsRef<Path>, header: IndexFileHeader) -> Result<Self> {
         let (sender, receiver) = channel::bounded(8);
         let file = File::options()
             .append(true)
-            .open(path.as_ref().join(format!("{file_name}.{suffix}")))?;
+            .create(true)
+            .open(path.as_ref())?;
 
         let file_size = file.metadata()?.len();
         ensure!(
-            file_size % size_of::<T>() as u64 == 0,
+            file_size == 0
+                || (file_size - size_of::<IndexFileHeader>() as u64) % size_of::<T>() as u64 == 0,
             "Invalid log index file"
         );
 
-        Ok(Self {
-            handle: thread::spawn(move || Self::exec(file, receiver)),
-            sender,
-        })
+        thread::spawn(move || Self::exec(file, file_size, receiver, header));
+
+        Ok(Self { sender })
     }
 
-    pub fn append_log_indexes(&self, indexes: Vec<T>) -> Result<()> {
-        if self.handle.is_finished() {
-            Err(anyhow!("Worker thread already exited"))?;
-        }
-
+    pub(crate) fn append_log_indexes(&self, indexes: Vec<T>) -> Result<()> {
         self.sender.send(indexes)?;
         Ok(())
     }
 
-    fn exec(mut index_file: File, receiver: Receiver<Vec<T>>) -> Result<()> {
+    fn exec(
+        mut index_file: File,
+        file_size: u64,
+        receiver: Receiver<Vec<T>>,
+        header: IndexFileHeader,
+    ) -> Result<()> {
+        Self::check_or_init_header(&mut index_file, file_size, header)?;
+
         while let Ok(indexes) = receiver.recv() {
             let mut buf = Vec::with_capacity(256);
 
@@ -62,5 +57,25 @@ impl<T: Serialize + Send + Sync + 'static> IndexWriter<T> {
         }
 
         Ok(())
+    }
+
+    fn check_or_init_header(
+        index_file: &mut File,
+        file_size: u64,
+        header: IndexFileHeader,
+    ) -> Result<()> {
+        if file_size == 0 {
+            index_file.write_all(&bincode::serialize(&header)?)?;
+            index_file.sync_data()?;
+
+            Ok(())
+        } else {
+            let mut buf = Box::new([0u8; size_of::<IndexFileHeader>()]);
+            index_file.read_at(0, buf.as_mut_slice())?;
+
+            (IndexFileHeader::try_from(buf.as_slice())? == header)
+                .then_some(())
+                .ok_or(anyhow!("Invalid file header"))
+        }
     }
 }
